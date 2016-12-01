4cc12ce8cf9bb1ee4758d1eeaece938fdb69cfdc
EuanRamsay <euancramsay@aol.com>
2016-12-01 14:16:45 +0000

    account created with name and amount

---
+++ account.js
@@ -0,0 +1,6 @@
+var Account = function(name, amount){
+  this.name = name;
+  this.amount = amount;
+}
+
+module.exports = Account;
---
+++ specs/account_spec.js
@@ -0,0 +1,20 @@
+var assert = require('assert');
+var Account = require('../account');
+
+var account;
+
+describe('Account', function() {
+
+  before(function() {
+    account = new Account("Mr Johnson", 5000.00)
+  })
+
+  it('test should have name', function() {
+    assert.equal('Mr Johnson', account.name);
+  });
+
+  it('test should have amount', function() {
+    assert.equal(5000.00, account.amount);
+  })
+
+});
