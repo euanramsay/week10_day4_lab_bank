b548d079465f27268cb210c16d9701226019c07d
EuanRamsay <euancramsay@aol.com>
2016-12-01 14:37:27 +0000

    findAccountByName method added to Bank

--- bank.js
+++ bank.js
@@ -9,6 +9,12 @@ Bank.prototype = {
   },
   addAccount: function(account) {
     this.accounts.push(account);
+  },
+  findAccountByName: function(name) {
+    var foundAccount = this.accounts.find(function(account) {
+      return account.name === name;
+    })
+    return foundAccount;
   }
 }
 
--- specs/bank_spec.js
+++ specs/bank_spec.js
@@ -2,12 +2,16 @@ var assert = require('assert');
 var Bank = require('../bank');
 
 var bank;
-var accountStub;
+var accountStub1;
+var accountStub2;
+var accountStub3;
 
 describe('Bank', function() {
 
   before(function() {
-    accountStub = {name: "Mr Johnson", amount: 5000, type: "personal"};
+    accountStub1 = {name: "Mr Johnson", amount: 5000, type: "personal"};
+    accountStub2 = {name: "Mr Max", amount: 2000, type: "personal"};
+    accountStub3 = {name: "Mr Euan", amount: 10000, type: "business"};
     bank = new Bank('Barclays');
   });
 
@@ -20,8 +24,35 @@ describe('Bank', function() {
   })
 
   it("should be able to add account", function() {
-    bank.addAccount(accountStub);
+    bank.addAccount(accountStub1);
     assert.equal(1, bank.accountCount());
   })
 
+  it("should be able to find account by name", function() {
+    bank.addAccount(accountStub1);
+    bank.addAccount(accountStub2);
+    bank.addAccount(accountStub3);
+    var foundAccount = bank.findAccountByName("Mr Max");
+    assert.equal(accountStub2, foundAccount);
+  })
+
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
